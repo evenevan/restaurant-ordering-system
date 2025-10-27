753bcf6fbd8dd1372588daeaa40b53ba392bd9e1
Evan F <84680760+Attituding@users.noreply.github.com>
2025-10-26 19:26:50 -0700

    chore: update configs

--- frontend/cypress/e2e/order.cy.ts
+++ frontend/cypress/e2e/order.cy.ts
@@ -2,7 +2,9 @@ describe('Home Page', () => {
   it('can place and receive order', () => {
     cy.visit('/');
 
-    cy.contains('Order Food').click();
+    cy.contains('Order Food', {
+      timeout: 10_000, // wait for app to load
+    }).click();
     cy.focused().type('Pizza{enter}');
     cy.contains('Order placed successfully').should('be.visible');
     cy.contains('Table 1').parent().parent().contains('Pending').should('be.visible');
@@ -10,7 +12,11 @@ describe('Home Page', () => {
 
 
     cy.contains('Status: Received', {
-      timeout: 17500, // max order time + a few seconds
+      timeout: 20_000, // max order time is 15 sec currently, + a few seconds
     }).should('be.visible');
+
+    cy.contains('Table 2').parent().parent().contains('No orders yet.').should('be.visible');
+    cy.contains('Table 3').parent().parent().contains('No orders yet.').should('be.visible');
+    cy.contains('Table 4').parent().parent().contains('No orders yet.').should('be.visible');
   })
 })
